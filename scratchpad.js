4936c34eb2b22c6366804321f5c44aec49a9bd93
Richard Loveland <ricky.loveland96@gmail.com>
2017-03-12 21:10:45 +0000

    added the tribonnaci sequence

--- scratchpad.js
+++ scratchpad.js
@@ -29,3 +29,19 @@ var pell = function pellHelper(number) {
     return returnNumber;
                     
 };
+
+var tri = function triHelper(number){
+  
+  var returnNumber
+  
+  if(number <= 1){
+    returnNumber = 0;
+  } else if (number === 2){
+    returnNumber = 1;
+  } else {
+    returnNumber = tri(number-1) + tri(number-2) + tri(number-3);
+  } 
+  
+  return returnNumber;
+  
+};
